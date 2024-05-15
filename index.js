785d2b6bc036564397bc2bd6bdf093ae0a2db364
Clovis Luiz <clovisluizsjr@gmail.com>
2024-05-15 20:24:17 -0300

    indicando o caminho para o vercel

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from path;
 
 const porta = 3000;
 const host = '0.0.0.0';
@@ -6,7 +7,7 @@ const host = '0.0.0.0';
 const app = express();
 var listaAlunos = [];
 
-app.use(express.static('./public'));
+app.use(express.static(path.join(process.cwd(),'public')));
 
 app.use('/cadastrarAluno', (req,resp)=>{
     const nome = req.query.nome;

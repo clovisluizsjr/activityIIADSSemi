55d0eccbebce903d95c31cf2f47c38377f737714
Clovis Luiz <clovisluizsjr@gmail.com>
2024-05-14 22:01:42 -0300

    desenvolvendo as páginas html

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const porta = 3000;
+const host = '0.0.0.0';
+
+const app = express();
+
+app.use(express.static('./public'));
+
+app.listen(porta, host, () => {
+    console.log(`Servidor executando na porta http://${host}:${porta}`);
+})
